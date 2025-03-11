6b39290b9126618ef3d505a82b66e9a87f219371
fujimiya007 <43722@prot.ac.th>
2025-03-11 14:38:38 +0700

    Add files via upload

---
+++ cell04/calculator.py
@@ -0,0 +1,9 @@
+# รับข้อมูลตัวเลขจากผู้ใช้
+num1 = float(input("Give me the first number: "))
+num2 = float(input("Give me the second number: "))
+
+# แสดงผลการคำนวณ
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
