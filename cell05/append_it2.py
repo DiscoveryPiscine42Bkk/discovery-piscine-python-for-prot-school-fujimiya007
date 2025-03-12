b9f783a2472ad3ff86321fd58c601e5ddd90885e
fujimiya007 <43722@prot.ac.th>
2025-03-12 10:27:58 +0700

    Add files via upload

---
+++ cell05/append_it2.py
@@ -0,0 +1,20 @@
+
+import sys
+
+def append_ism(parameters):
+    """
+    เพิ่ม "ism" ต่อท้ายแต่ละพารามิเตอร์ที่ไม่ลงท้ายด้วย "ism"
+    """
+
+    if not parameters:
+        print("none")
+        return
+
+    for param in parameters:
+        if not param.endswith("ism"):
+            print(param + "ism")
+
+if __name__ == "__main__":
+    parameters = sys.argv[1:]
+
+    append_ism(parameters)
